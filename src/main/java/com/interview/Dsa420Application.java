1534093cc5c71ebea266fe5673242e58e982bc52
Sanjay Patel <sanpatel@outlook.in>
2024-06-28 17:28:35 +0530

    FIrst Commit DSA

---
+++ src/main/java/com/interview/Dsa420Application.java
@@ -0,0 +1,13 @@
+package com.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsa420Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsa420Application.class, args);
+	}
+
+}
